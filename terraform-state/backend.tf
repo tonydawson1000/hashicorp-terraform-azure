b19536c8d453cf0ab548ab76dea6a533819f40db
Tony Dawson <tonydawson1000@gmail.com>
2023-01-27 17:19:38 +0000

    Tidyup of terraform-state resources folder

--- terraform-state/backend.tf
+++ terraform-state/backend.tf
@@ -3,14 +3,9 @@ terraform {
     resource_group_name = "TFState"
 
     #####
-    # "storage_container_name": "Visual Studio Enterprise – MPN"
+    # Add the 'storage_account_name' (with the random name) from the output here 
     #####
-    storage_account_name = "tfstatestaters5ljv"
-
-    #####
-    # "storage_container_name": "Visual Studio Enterprise"
-    ##### 
-    #    storage_account_name = "<TO-DO>"
+    storage_account_name = "tfstate2y6ck5"
 
     container_name = "terraform-state-tfstate"
     key            = "terraform.tfstate"
--- terraform-state/output.tf
+++ terraform-state/output.tf
@@ -5,6 +5,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.tfstate.name
 }
 
+output "resource_group_location" {
+  value = azurerm_resource_group.tfstate.location
+}
+
 output "storage_account_name" {
   value = azurerm_storage_account.tfstate.name
 }
